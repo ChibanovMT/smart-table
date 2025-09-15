a51e70738b3fbd282a9c89b2a67346292d86372f
Matvey Chibanov <mtchibanov@yandex.ru>
2025-09-15 15:14:15 +0300

    Всё готово

---
+++ smart-table-main/src/components/sorting.js
@@ -0,0 +1,34 @@
+import {sortMap} from "../lib/sort.js"; // sortCollection больше не нужен
+
+export function initSorting(columns) {
+    return (query, state, action) => {
+        let field = null;
+        let order = null;
+
+        if (action && action.name === 'sort') {
+            // @todo: #3.1 — запомнить выбранный режим сортировки
+            action.dataset.value = sortMap[action.dataset.value || 'none'];    // Сохраним и применим как текущее следующее состояние из карты
+            field = action.dataset.field;                                      // Информация о сортируемом поле есть также в кнопке
+            order = action.dataset.value;                                      // Направление заберём прямо из датасета для точности
+
+            // @todo: #3.2 — сбросить сортировки остальных колонок
+            columns.forEach((column) => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
+        } else {
+            // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach((column) => {
+                if ((column.dataset.value || 'none') !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
+        }
+
+        const sort = (field && order !== 'none') ? `${field}:${order}` : null; // сохраним в переменную параметр сортировки в виде field:direction
+
+        return sort ? Object.assign({}, query, { sort }) : query; // по общему принципу, если есть сортировка, добавляем, если нет, то не трогаем query
+    }
+}
